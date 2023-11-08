2c5766ad2e65e0c4b4fdb4b2e1af9d0976065d3f
Ramiro Martin <ramiro_martin@hotmail.com>
2023-11-08 15:01:11 -0300

    error boundary added - form to Form

--- app/root.tsx
+++ app/root.tsx
@@ -2,12 +2,14 @@
 import type { LinksFunction } from "@remix-run/node";
 import MainNavigation from "~/components/MainNavigation";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
 import styles from "~/styles/main.css";
 
@@ -19,8 +21,6 @@ import styles from "~/styles/main.css";
 //       ]
 //     : []),
 // ];
-
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export default function App() {
   return (
@@ -43,3 +43,35 @@ export default function App() {
     </html>
   );
 }
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+        <title>An Error Ocurred</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main className="error">
+          <h1>An Error Ocurred!</h1>
+          <p>{error.message}</p>
+          <p>
+            Back to <Link to="/">safety</Link>!
+          </p>
+        </main>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,5 +1,5 @@
 import { redirect } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData, useRouteError } from "@remix-run/react";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
 import NoteList, { links as noteListLinks } from "~/components/NoteList";
 import { getStoredNotes, storeNotes } from "~/data/notes";
@@ -22,6 +22,11 @@ export default function NotesPage() {
 export async function action({ request }) {
   const formData = await request.formData();
   const noteData = Object.fromEntries(formData);
+
+  // Add validation
+  if (noteData.title.trim().length < 5) {
+    return { message: "Invalid title, must be at least 5 characters long." };
+  }
   const existingNotes = await getStoredNotes();
 
   noteData.id = new Date().toISOString();
@@ -30,10 +35,30 @@ export async function action({ request }) {
 
   await storeNotes(updatedNotes);
 
+  // JUST TO PERFORM A FALSE DELAY IN THE BACKEND CALL
+  // await new Promise((resolve, reject) =>
+  //   setTimeout(() => {
+  //     resolve();
+  //   }, 2000)
+  // );
+
   return redirect("/notes");
 }
 
 export async function loader() {
   const notes = await getStoredNotes();
   return notes;
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  return (
+    <main className="error">
+      <h1>An Error related with your notes ocurred!</h1>
+      <p>{error.message}</p>
+      <p>
+        Back to <Link to="/">safety</Link>!
+      </p>
+    </main>
+  );
 }
